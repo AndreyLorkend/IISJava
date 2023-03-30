7b57ed5839815866ab3169408696d5414d160c56
AndreyLorkend <andrei190164@gmail.com>
2023-03-30 10:27:12 +0400

    init repos

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.iisjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.iisjava to javafx.fxml;
+    exports com.example.iisjava;
+}
